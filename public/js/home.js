061c05b8fcef7dbbdc052af716c5b7a16b6806b1
sama <catslinsama@gmail.com>
2023-10-23 18:52:08 +0800

    test

--- public/js/home.js
+++ public/js/home.js
@@ -6,14 +6,12 @@ let isExpanded = false; // 初始状态为未展开
 
 avatarActive.onclick = function () {
   if (isExpanded) {
-    // 如果已展开，则恢复原始状态
     introduction.style.width = "5%";
     information.style.display = "none";
   } else {
-    // 如果未展开，则设置为20%宽度
     introduction.style.width = "20%";
     information.style.display = "block";
   }
-  
-  isExpanded = !isExpanded; // 切换状态
+
+  isExpanded = !isExpanded;
 };
